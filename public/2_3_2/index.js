ab5cfce0c70a1cb7bacb3456784b50f9ff9110d7
antonshust <133322472+antonshust@users.noreply.github.com>
2024-11-30 14:40:46 +0600

    Работа готова

--- public/2_3_2/index.js
+++ public/2_3_2/index.js
@@ -1,4 +1,4 @@
-import Gallery from '../../src/2/3/Gallery';
+import Gallery from '../../src/2/3/Gallery2';
 import { createRoot } from 'react-dom/client';
 
 const root = createRoot(document.getElementById('root'))
--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles.css'
 
-import App from './2/7/App10'
+import App from './2/3/Gallery2'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
